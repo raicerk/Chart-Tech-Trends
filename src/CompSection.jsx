44c7d169d6c0645f8b4766b66a95d0d3e102fcf0
raicerk <raicerk@gmail.com>
2019-09-26 17:49:56 -0300

    Se modifica logica del sistema para que rescate y de formato a los datos

--- src/CompSection.jsx
+++ src/CompSection.jsx
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
 import CompGraficos from './CompGraficos';
 import Axios from 'axios';
+import config from './config.json';
 
 class CompSection extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            data1: []
+            data: [],
+            dataLenguajes: [],
+            dataBaseDeDatos: []
         };
-      }
+    }
 
 
     componentDidMount() {
-        Axios.get(`http://www.mocky.io/v2/5d8cda532e00005100abdd8f`).then(res => {
-            this.setState({ data1: res.data })
+        Axios.get(config.urlDataLaboral).then(res => {
+            // let dataLenguaje = res.data.data.LaboralAgrupadoPorMes.map(iter => {
+            //     return {
+            //         "id": iter.skill,
+            //         "data": iter.datos.map(i => {
+            //             return {
+            //                 "x": i.fecha,
+            //                 "y": iter.cantidad
+            //             }
+            //         })
+            //     }
+            // })
+
+            console.log(res.data.data)
+
+
+            //this.setState({ dataLenguajes: dataLenguaje })
         }).catch(error => {
             console.log(error)
         })
@@ -24,11 +42,11 @@ class CompSection extends Component {
         return (
             <div className="contenido">
 
-                <CompGraficos data={this.state.data1} />
+                <CompGraficos data={this.state.dataLenguajes} />
                 <p>
                     Lorea El Ipsum Washas gila saque embarao brocacochi zoronca hermano zorra de vioh, saque del corte para la mano hermano zarpao brocacochi chantar querí ser leyenda, readi saque pero pa q po de vioh chantale el pate oe zi terrible de perkin. Qliao paquepo zarpao chantar asikalao saque truco machucao te tiraste, odio tenis mano? cuca rati querí ser leyenda choro buqué de vioh, tenis mano? de vioh terrible de perkin matagatos de vioh de corte pasa paca coshino ql.
                 </p>
-                <CompGraficos data={this.state.data1} />
+                <CompGraficos data={this.state.data} />
                 <p>
                     Lorea El Ipsum Washas gila saque embarao brocacochi zoronca hermano zorra de vioh, saque del corte para la mano hermano zarpao brocacochi chantar querí ser leyenda, readi saque pero pa q po de vioh chantale el pate oe zi terrible de perkin. Qliao paquepo zarpao chantar asikalao saque truco machucao te tiraste, odio tenis mano? cuca rati querí ser leyenda choro buqué de vioh, tenis mano? de vioh terrible de perkin matagatos de vioh de corte pasa paca coshino ql.
                 </p>
