fe3feca4f9eccaa6e6cf0f80a8997017a842f3cc
raicerk <raicerk@gmail.com>
2019-09-26 15:52:01 -0300

    Se modifica logica de componente que carga los datos

--- src/CompSection.jsx
+++ src/CompSection.jsx
@@ -4,11 +4,17 @@ import Axios from 'axios';
 
 class CompSection extends Component {
 
-    data1 = [];
+    constructor(props) {
+        super(props);
+        this.state = {
+            data1: []
+        };
+      }
+
 
     componentDidMount() {
         Axios.get(`http://www.mocky.io/v2/5d8cda532e00005100abdd8f`).then(res => {
-            this.data1 =  res.data
+            this.setState({ data1: res.data })
         }).catch(error => {
             console.log(error)
         })
@@ -17,12 +23,12 @@ class CompSection extends Component {
     render() {
         return (
             <div className="contenido">
-                
-                <CompGraficos data={this.data1} />
+
+                <CompGraficos data={this.state.data1} />
                 <p>
                     Lorea El Ipsum Washas gila saque embarao brocacochi zoronca hermano zorra de vioh, saque del corte para la mano hermano zarpao brocacochi chantar querí ser leyenda, readi saque pero pa q po de vioh chantale el pate oe zi terrible de perkin. Qliao paquepo zarpao chantar asikalao saque truco machucao te tiraste, odio tenis mano? cuca rati querí ser leyenda choro buqué de vioh, tenis mano? de vioh terrible de perkin matagatos de vioh de corte pasa paca coshino ql.
                 </p>
-                <CompGraficos data={this.data1} />
+                <CompGraficos data={this.state.data1} />
                 <p>
                     Lorea El Ipsum Washas gila saque embarao brocacochi zoronca hermano zorra de vioh, saque del corte para la mano hermano zarpao brocacochi chantar querí ser leyenda, readi saque pero pa q po de vioh chantale el pate oe zi terrible de perkin. Qliao paquepo zarpao chantar asikalao saque truco machucao te tiraste, odio tenis mano? cuca rati querí ser leyenda choro buqué de vioh, tenis mano? de vioh terrible de perkin matagatos de vioh de corte pasa paca coshino ql.
                 </p>
