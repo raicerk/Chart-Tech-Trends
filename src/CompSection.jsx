e6e232548f43cd4480d44d91593c36e0cc932b19
raicerk <raicerk@gmail.com>
2019-09-30 17:28:51 -0300

    Se agregan nuevos graficos para la aplicacion

--- src/CompSection.jsx
+++ src/CompSection.jsx
@@ -18,6 +18,7 @@ class CompSection extends Component {
 
     componentDidMount() {
 
+        // Lenguajes de programacion
         Axios.post(config.urlGraphQL, {
             query:`{
                 LaboralAgrupadoPorMes(where: {field: "pais",value: "CL"}){
@@ -63,6 +64,46 @@ class CompSection extends Component {
         }).catch(error => {
             console.log(error)
         })
+
+
+        // Bases de datos
+        Axios.post(config.urlGraphQL, {
+            query:`{
+                LaboralAgrupadoPorMes(where: {field: "pais",value: "CL"}){
+                    skill
+                    datos{
+                        fecha
+                        cantidad
+                    }
+                }
+            }`
+        }).then(res => {
+            let dataBaseDeDatos = res.data.data.LaboralAgrupadoPorMes.filter(iter =>
+                iter.skill === "MongoDB" ||
+                iter.skill === "MySQL" ||
+                iter.skill === "NoSQL" ||
+                iter.skill === "Oracle" ||
+                iter.skill === "Oracle DB" ||
+                iter.skill === "PostgreSQL" ||
+                iter.skill === "Redis" ||
+                iter.skill === "SQL"
+            ).map(iter => {
+                return {
+                    "id": iter.skill,
+                    "data": iter.datos.sort().map(i => {
+                        let fec = i.fecha.split("-")
+                        return {
+                            "x": `${fec[0]}${fec[1]}`,
+                            "y": i.cantidad
+                        }
+                    })
+                }
+            })
+            this.setState({ dataBaseDeDatos: dataBaseDeDatos })
+        }).catch(error => {
+            console.log(error)
+        })
+
     }
 
     render() {
@@ -70,6 +111,11 @@ class CompSection extends Component {
             <div className="contenido">
 
                 <CompGraficos data={this.state.dataLenguajes} />
+                <p>
+                    Lorea El Ipsum Washas gila saque embarao brocacochi zoronca hermano zorra de vioh, saque del corte para la mano hermano zarpao brocacochi chantar querí ser leyenda, readi saque pero pa q po de vioh chantale el pate oe zi terrible de perkin. Qliao paquepo zarpao chantar asikalao saque truco machucao te tiraste, odio tenis mano? cuca rati querí ser leyenda choro buqué de vioh, tenis mano? de vioh terrible de perkin matagatos de vioh de corte pasa paca coshino ql.
+                </p>
+
+                <CompGraficos data={this.state.dataBaseDeDatos} />
                 <p>
                     Lorea El Ipsum Washas gila saque embarao brocacochi zoronca hermano zorra de vioh, saque del corte para la mano hermano zarpao brocacochi chantar querí ser leyenda, readi saque pero pa q po de vioh chantale el pate oe zi terrible de perkin. Qliao paquepo zarpao chantar asikalao saque truco machucao te tiraste, odio tenis mano? cuca rati querí ser leyenda choro buqué de vioh, tenis mano? de vioh terrible de perkin matagatos de vioh de corte pasa paca coshino ql.
                 </p>
