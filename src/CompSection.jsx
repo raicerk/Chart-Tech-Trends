8ed32c45713bd525ec9a9502da6901cbb8643110
raicerk <raicerk@gmail.com>
2019-10-08 15:22:02 -0300

    Se agrega seleccionador de skill

--- src/CompSection.jsx
+++ src/CompSection.jsx
@@ -13,39 +13,39 @@ class CompSection extends Component {
         return (
             <div className="contenido">
 
-                <hr/>
-                <OtherSkillGraph/>
-                <hr/>
-                
-                <hr/>
+                <hr />
+                <OtherSkillGraph />
+                <hr />
+
+                <hr />
                 <LanguageAcumulatedGraph />
                 <LanguageGraph />
-                <LanguajeSalaryGraph/>
-                <hr/>
+                <LanguajeSalaryGraph />
+                <hr />
 
-                <hr/>
-                <DatabaseAcumulatedGraph/>
+                <hr />
+                <DatabaseAcumulatedGraph />
                 <DatabaseGraph />
-                <DatabaseSalaryGraph/>
-                <hr/>
+                <DatabaseSalaryGraph />
+                <hr />
 
-                <hr/>
-                <FrameworkJSAcumulatedGraph/>
+                <hr />
+                <FrameworkJSAcumulatedGraph />
                 <FrameworkJSGraph />
-                <FrameworkJSSalaryGraph/>
-                <hr/>
+                <FrameworkJSSalaryGraph />
+                <hr />
 
-                <hr/>
-                <CloudServicesSAcumulatedGraph/>
+                <hr />
+                <CloudServicesSAcumulatedGraph />
                 <CloudServicesGraph />
-                <CloudServicesSalaryGraph/>
-                <hr/>
+                <CloudServicesSalaryGraph />
+                <hr />
 
-                <hr/>
-                <MobileAcumulatedGraph/>
+                <hr />
+                <MobileAcumulatedGraph />
                 <MobileGraph />
-                <MobileSalaryGraph/>
-                <hr/>
+                <MobileSalaryGraph />
+                <hr />
             </div>
         );
     }
@@ -316,6 +316,7 @@ const LanguageAcumulatedGraph = () => {
                     }
                 }`
         }).then(res => {
+            console.log(JSON.stringify(res.data.data.LaboralAcumulado.map(iter=>iter.skill)))
             const dataLenguajeAcumulado = res.data.data.LaboralAcumulado.filter(iter =>
                 iter.skill === "C" ||
                 iter.skill === "C#" ||
@@ -704,36 +705,48 @@ const MobileSalaryGraph = () => {
 const OtherSkillGraph = () => {
     const context = useContext(AppContext)
     const [data, setData] = useState([])
+    const [skill, setSkill] = useState('.Net')
+    let skills = ['.Net','3D','ABAP','AI','API','ASP','Access Point','ActionScript','Adobe Suite','Advertising','Agile','Amazon Web Services','Analytics','Android','Angular 2','Angular 4','Angular 5','Angular 6','AngularJS','Apache Spark','Arquitectura de Información','Artificial Intelligence','Asterisk','Axios','Azure','Backbone.js','Big Data','BigQuery','Blockchain','Bootstrap','Branding','Business Intelligence','C','C#','C++','CEO','CMS','CRM','CSS','CTO','Cake PHP','Celery','CircleCi','Cloud Computing','CodeIgniter','Computer vision','Continuous Deployment','Continuous Integration','Copywriting','Cordova','Customer Service','Data Analysis','Data Science','Data Transformation','Deep Learning','Design Thinking','DevOps','Digital Planning','Dirección de Arte','Django','Docker','Drupal','E-commerce','ES2017','ES6','ETL','Ejecutivo de Cuentas','Elasticsearch','Elixir','Ember.js','Erlang','Excel','Express','Facebook Ads','Flask','Front-End','Full-stack','Functional Programming','GIS','Git','Go','Golang','Google Adwords','Google App Engine','GraphQL','Graphic Design','Groovy','Growth','HTML5','Hadoop','Head of Art','ITIL','Industrial Design','Ingeniero Comercial','Internet of Things','Invision','IoT','Ionic','JEE','JSF','JSON','JasperReports','Java','JavaScript','Jenkins','Kanban','Kotlin','Kubernetes','Laravel','Lean','Liferay','Linux','MVC','Machine Learning','Magento','Marketing','Material Design','Matlab','Maven','Media Planning','Meteor','Microservices','MongoDB','Motion Graphics','MySQL','NPM','Natural Language Processing','Nginx','NoSQL','Node.js','OSB','Objective-C','Odoo','Openstack','Oracle','Oracle DB','PHP','Perl','PhoneGap','Photoshop','Planificación de Medios','Planning','PostgreSQL','Power BI','Prestashop','Product Manager','Project Management','Project Manager','Python','QA','QA-Automation','R','REST','REST API','RabbitMQ','React','React-Native','Redis','Redux','Responsive','Ruby','Ruby on Rails','SEM','SEO','SOA','SPA','SQL','SVN','SaaS','Sails.js','Salesforce','Sap','Scala','Scrum','Selenium','Service Design','Service Desk','Shopify','Sidekiq','Sketch','Social Media','Soporte técnico','Spark','Spring','Strategic Design','Swift','Switching','Symfony','Sysadmin','TDD','Telefonía','Tornado','Trafficker','TypeScript','UI Design','UML','UX','Unity','Usabilidad','User Research','VTEX','Varnish','Ventas','Video Games Development','Virtualization','Web design','Web server','Websphere','Wireframes','WordPress','Xamarin','Yii2','Zend','iOS','jQuery','kotlin','objective c','vue.js']
 
     useEffect(() => {
-        
-        let variablefromselect = 'Java'
 
+        
         Axios.post(config.urlGraphQL, {
             query: `{
-                LaboralConOtrosSkill(country: {value: "${context.pais}"}, skill: {value: "${variablefromselect}"}){
+                LaboralConOtrosSkill(country: {value: "${context.pais}"}, skill: {value: "${skill}"}){
                     skill
                     cantidad
                 }
             }`
         }).then(res => {
-            const datOtherSkill = res.data.data.LaboralConOtrosSkill.filter(iter => iter.skill !== variablefromselect).slice(0,10).sort( (a, b)=> {
+            const datOtherSkill = res.data.data.LaboralConOtrosSkill.filter(iter => iter.skill !== skill).slice(0, 10).sort((a, b) => {
                 if (a.cantidad > b.cantidad) {
-                  return 1;
+                    return 1;
                 }
                 if (a.cantidad < b.cantidad) {
-                  return -1;
+                    return -1;
                 }
                 return 0;
-              })
+            })
             setData(datOtherSkill)
         }).catch(error => {
             console.log(error)
         })
-    }, [context.pais])
+    }, [context.pais, skill])
 
     return (
-        <CompGraficoBarraHorizontal data={data} />
+        <div>
+            <select onChange={(e) => { setSkill(e.target.value) }} >
+                {
+                    skills.map((iter, index) =>
+                        <option key={String(index)} value={iter}>
+                            {iter}
+                        </option>
+                    )
+                }
+            </select>
+            <CompGraficoBarraHorizontal data={data} />
+        </div>
     )
 }
 
