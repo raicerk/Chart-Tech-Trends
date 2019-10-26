16eb21542d1b2cc7f621df8296b19df873d247fc
Jose Lezama <joseglego@gmail.com>
2019-10-26 19:52:58 -0300

    feat(Home): cambio de imagen del Home

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
         <li>Chequear el Código de FrontEnd en <a href="https://github.com/raicerk/Chart-Tech-Trends" target="_blank" rel="noopener noreferrer">Chart-Tech-Trends</a></li>
         <li>Chequear el Código de Backend en <a href="https://github.com/pgramadores/LaboralGraphQL" target="_blank" rel="noopener noreferrer">LaboralGraphQL</a></li>
         <li>Proyecto de <a href="https://github.com/raicerk">Juan Mora</a></li>
-        <li>Foto de <a href="https://unsplash.com/@schwiet?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Seth Schwiet</a> descargada de <a href="/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></li>
+        <li>Foto de <a href="https://unsplash.com/@ffstop?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Fotis Fotopoulos</a> descargada de <a href="/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></li>
       </ul>     
     </div>
   );
