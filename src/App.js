07002c3e7c7d508c45d37f208120514c43d35d9b
Jose Lezama <joseglego@gmail.com>
2019-10-27 12:46:32 -0300

    refactor(CSSApp): reescribir css como scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import Header from './Header/Header';
 import Home from './Home/Home';
 import Section from './Section/Section';
