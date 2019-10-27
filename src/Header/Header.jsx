5b9f5d4db541501d2f21542a99f53c1cc4c75f30
Jose Lezama <joseglego@gmail.com>
2019-10-26 21:49:56 -0300

    feat(Header): mostrar/esconder header en base al scroll

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,11 +1,25 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import CompPaises from '../CompPaises';
 
 import './Header.css';
 
 function Header() {
+  const [extraClass, setExtraClass] = useState('no-header')
+
+  const handleScroll = () => {
+    const offset = 500;
+    const windowsScrollTop  = window.pageYOffset;
+
+    setExtraClass(windowsScrollTop >= offset ? '' : 'no-header')
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return window.addEventListener('scroll', handleScroll);
+  })
+
   return (
-    <header className="header">
+    <header className={`header ${extraClass}`}>
       <h1 className="header__title">
         Tech
         <span className='header-title__second'>Trends</span>
