c58d80224ce64b1ec8830d08878cc2c21f688cf7
raicerk <raicerk@gmail.com>
2019-10-27 22:11:47 -0300

    Se conviente clase componente principal a component funcional

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './Header/Header';
 import Home from './Home/Home';
@@ -6,34 +6,25 @@ import Section from './Section/Section';
 import AppContext from './AppContext';
 import Footer from './Footer/Footer';
 
-class App extends Component {
+function App() {
 
-  state = {
-    pais: "CL"
-  }
+  const [pais, handleSetPais] = useState("CL")
 
-  handleSetPais = (Pais) => {
-    this.setState({ pais: Pais })
-  }
-
-  render() {
-
-    return (
-      <AppContext.Provider value={{ 
-        pais: this.state.pais,
-        setPais: this.handleSetPais
-       }}>
-        <div className="App">
-          <Header />
-          <main>
-            <Home />
-            <Section />
-          </main>
-          <Footer />
-        </div>
-      </AppContext.Provider>
-    );
-  }
+  return (
+    <AppContext.Provider value={{
+      pais: pais,
+      setPais: handleSetPais
+    }}>
+      <div className="App">
+        <Header />
+        <main>
+          <Home />
+          <Section />
+        </main>
+        <Footer />
+      </div>
+    </AppContext.Provider>
+  );
 }
 
 export default App;
