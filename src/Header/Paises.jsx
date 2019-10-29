b5554723d52411b64ebb9c62b691e1328b14bce9
noandersonls <alljedicake@gmail.com>
2019-10-29 07:15:48 -0300

    feature: persist countries label for a11y

--- src/Header/Paises.jsx
+++ src/Header/Paises.jsx
@@ -1,23 +1,30 @@
-import React  from 'react';
+import React from 'react';
 import { SingleSelect } from "react-select-material-ui";
 import AppContext from '../AppContext';
 
-const COUNTRYCODES = ['CL', 'AR', 'PE', 'MX', 'CO']
+const countries = [
+  { label: "Chile", value: "CL" },
+  { label: "Argentina", value: "AR" },
+  { label: "Perú", value: "PE" },
+  { label: "México", value: "MX" },
+  { label: "Colombia", value: "CO" }
+];
+
 
 const Paises = React.memo((props) => (
-    <AppContext.Consumer>
-        {(context) => (
-            <div className="paises">
-              <SingleSelect
-                defaultValue='CL'
-                placeholder="País" 
-                options={COUNTRYCODES}
-                onChange={(pais) => context.setPais(pais)}   
-                style={{ width: 100 }}  
-              />
-            </div>
-        )}
-    </AppContext.Consumer>
-))
+      <AppContext.Consumer>
+          {(context) => (
+              <div className="paises">
+                <SingleSelect
+                  placeholder="País" 
+                  options={countries}
+                  onChange={country => context.setPais(country)}   
+                  style={{ width: 100 }}  
+                />
+              </div>
+          )}
+      </AppContext.Consumer>
+    )
+)
 
 export default Paises;
