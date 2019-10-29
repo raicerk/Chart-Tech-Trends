82b5a8bf134a32357106ae0d3ba5c86db0116666
noandersonls <alljedicake@gmail.com>
2019-10-28 21:24:38 -0300

    feature: change stateless class components

--- src/Section/CompGraficoBarraHorizontal.jsx
+++ src/Section/CompGraficoBarraHorizontal.jsx
@@ -1,45 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ResponsiveBar } from '@nivo/bar';
 import config from '../config.json';
 import PropTypes from 'prop-types';
 
-class CompGraficoBarraHorizontal extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            other: ""
-        };
-    }
+const CompGraficoBarraHorizontal = ({ data }) => {
+  return (
+    <div className="grafico">
+        <ResponsiveBar
+            data={data}
+            indexBy={'skill'}
+            margin={config.chart.margin}
+            colors={{ scheme: 'nivo' }}
+            borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+            labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+            keys={['cantidad']}
+            axisBottom={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: 'Cantidad de Skill en ofertas laborales',
+                legendPosition: 'middle',
+                legendOffset: 32
+            }}
+            padding={0.2}
+            layout="horizontal"
+            enableGridY={false}
+            enableGridX={true}
+            legends={config.chart.legens}
 
-    render() {
-        return (
-            <div className="grafico">
-                <ResponsiveBar
-                    data={this.props.data}
-                    indexBy={'skill'}
-                    margin={config.chart.margin}
-                    colors={{ scheme: 'nivo' }}
-                    borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
-                    labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
-                    keys={['cantidad']}
-                    axisBottom={{
-                        tickSize: 5,
-                        tickPadding: 5,
-                        tickRotation: 0,
-                        legend: 'Cantidad de Skill en ofertas laborales',
-                        legendPosition: 'middle',
-                        legendOffset: 32
-                    }}
-                    padding={0.2}
-                    layout="horizontal"
-                    enableGridY={false}
-                    enableGridX={true}
-                    legends={config.chart.legens}
-
-                />
-            </div>
-        );
-    }
+        />
+    </div>
+  );
 }
 
 CompGraficoBarraHorizontal.propTypes = {
