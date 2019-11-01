3fd99e08419ac9298934f28988f8c2665366683e
raicerk <raicerk@gmail.com>
2019-11-01 18:14:57 -0300

    Ajuste de texto y css en footer

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -6,8 +6,8 @@ function Footer() {
   return (
     <div className="footer">
       <ul className="footer__resources">
-        <li>Chequear el Código de FrontEnd en <a href="https://github.com/raicerk/Chart-Tech-Trends" target="_blank" rel="noopener noreferrer">Chart-Tech-Trends</a></li>
-        <li>Chequear el Código de Backend en <a href="https://github.com/pgramadores/LaboralGraphQL" target="_blank" rel="noopener noreferrer">LaboralGraphQL</a></li>
+        <li>Código de FrontEnd en <a href="https://github.com/raicerk/Chart-Tech-Trends" target="_blank" rel="noopener noreferrer">Chart-Tech-Trends</a></li>
+        <li>Código de Backend en <a href="https://github.com/pgramadores/LaboralGraphQL" target="_blank" rel="noopener noreferrer">LaboralGraphQL</a></li>
         <li>Proyecto de <a href="https://github.com/raicerk">Juan Mora</a></li>
         <li>Foto de <a href="https://unsplash.com/@ffstop?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Fotis Fotopoulos</a> descargada de <a href="/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></li>
       </ul>     
